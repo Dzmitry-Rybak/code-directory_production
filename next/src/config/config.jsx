02252da3acfec1b7e3e9f60d83282cf89aabb1bb
Dzmitry Rybak <nakadisa@icloud.com>
2024-01-22 15:15:13 +0000

    test

--- next/src/config/config.jsx
+++ next/src/config/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: process.env.REACT_APP_API_URL || 'https://167.99.243.185:5000',
+    apiUrl: process.env.REACT_APP_API_URL || 'http://167.99.243.185:5000',
     socialLinks: {
         instagram: 'https://instagram.com/example'
     },
